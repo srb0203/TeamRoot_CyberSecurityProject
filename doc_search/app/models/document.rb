2302abcb24c479452ca7a29eaafe85b2b3382923
srb0203 <srb0203@gmail.com>
2016-11-06 13:24:27 -0600

    Saurabh: Changing the view for showing keywowrds on screen

--- doc_search/app/models/document.rb
+++ doc_search/app/models/document.rb
@@ -3,7 +3,7 @@ class Document < ActiveRecord::Base
     file_names = ['GeneratedText/Download.txt']
     file_names.each do |file_name|
       text = File.read(file_name)
-      new_contents = text.gsub(/[^0-9A-Za-z]/, '')
+      new_contents = text.gsub(/[^0-9A-Za-z ]/, '')
 
       # To write changes to the file, use:
       File.open('GeneratedText/Parsed_Doc.txt', "w") {|file| file.puts new_contents }
