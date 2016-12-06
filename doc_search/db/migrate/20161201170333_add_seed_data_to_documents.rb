60c733dbc789934f07e0c764328cb56fcba3ccbf
sudheer <dsudheer@gmail.com>
2016-12-06 04:45:53 +0000

    png data

--- doc_search/db/migrate/20161201170333_add_seed_data_to_documents.rb
+++ doc_search/db/migrate/20161201170333_add_seed_data_to_documents.rb
@@ -22,41 +22,32 @@ class AddSeedDataToDocuments < ActiveRecord::Migration
     end
 
   def up
-    document_params = { title: "Wifi security : wirless network encryption", author: "Gavin Garcia", doctype: "research paper", category:"cyber security", keywords:"wireless network encryption", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EthicalHackingProject.pdf" }
+    document_params = { title: "Wifi Security : Wirless Network Encryption", author: "Gavin Garcia", doctype: "Research Paper", category:"Cyber Security", keywords:"wireless network encryption", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EthicalHackingProject.pdf" }
     create(document_params)
-    document_params = {title: "Software Crackers", author: "Bryan Degro", doctype: "research paper", category:"cyber security", keywords:"cracker hacker", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Researchpaper.pdf" }        
+    document_params = {title: "Software Crackers", author: "Bryan Degro", doctype: "Research Paper", category:"Cyber Security", keywords:"Cracker Hacker", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Researchpaper.pdf" }        
     create(document_params)
-    document_params = {title: "Mobile Malware", author: "Alexander Pons", doctype: "research paper", category:"cyber security", keywords:"malware mobile", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Ethical%20Hacking%20final%20paper%202.0.pdf"}
+    document_params = {title: "Mobile Malware", author: "Alexander Pons", doctype: "Research Paper", category:"Cyber Security", keywords:"Malware Mobile", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Ethical%20Hacking%20final%20paper%202.0.pdf"}
     create(document_params)
-    document_params = {title: "Smart Grid", author: "Oniel Gordon", doctype: "research paper", category:"cyber security", keywords:"smart grid", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Smart%20Grid%20cyber%20physical%20security.pdf" }
+    document_params = {title: "Smart Grid", author: "Oniel Gordon", doctype: "Research Paper", category:"Cyber Security", keywords:"Smart Grid", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Smart%20Grid%20cyber%20physical%20security.pdf" }
     create(document_params)
-    document_params = {title: "The Insights into car hacking", author: "Mark Bacchus", doctype: "research paper", category:"cyber security", keywords:"car hacking", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Car%20Hacking%20-%20eel%206931%20final.pdf"}
+    document_params = {title: "The Insights Into Car Hacking", author: "Mark Bacchus", doctype: "Research Paper", category:"Cyber Security", keywords:"car hacking", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Car%20Hacking%20-%20eel%206931%20final.pdf"}
     create(document_params)
-    document_params = {title: "Ethical hacking and counter measures", author: "Pons", doctype: "research paper", category:"cyber security", keywords:"ethical hacking", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EthicalHackingFinalDraft5PageProposal.pdf" }
+    document_params = {title: "Ethical Hacking And Counter Measures", author: "Pons", doctype: "Research Paper", category:"Cyber Security", keywords:"ethical hacking", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EthicalHackingFinalDraft5PageProposal.pdf" }
     create(document_params)
-    document_params = {title: "Cloud Computing", author: "Roberto Calderon", doctype: "research paper", category:"cyber security", keywords:"cloud computing", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Ethical%20Hacking%20Group%20Proposal%20Final.pdf"}
+    document_params = {title: "Cloud Computing", author: "Roberto Calderon", doctype: "Research Paper", category:"Cyber Security", keywords:"cloud computing", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Ethical%20Hacking%20Group%20Proposal%20Final.pdf"}
     create(document_params)
-    document_params = {title: "Hardware and Software security", author: "DaQuan Stevens", doctype: "research paper", category:"cyber security", keywords:"software hardware security", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Hardware%20and%20Software%20Security.pdf"}
+    document_params = {title: "Hardware And Software Security", author: "DaQuan Stevens", doctype: "Research Paper", category:"Cyber Security", keywords:"software hardware security", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Hardware%20and%20Software%20Security.pdf"}
     create(document_params)
-    document_params = {title: "Pharming", author: "Osvaldo Concepcion", doctype: "research paper", category:"cyber security", keywords:"pharming phishing", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EEL4789_Project.pdf"}
+    document_params = {title: "Pharming", author: "Osvaldo Concepcion", doctype: "Research Paper", category:"Cyber Security", keywords:"pharming phishing", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EEL4789_Project.pdf"}
     create(document_params)
-    document_params = {title: "Creating Exploits", author: "Mark Bacchus", doctype: "research paper", category:"cyber security", keywords:"exploits", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Creating%20Exploits%20-%20EEL%206931.pdf"}
+    document_params = {title: "Creating Exploits", author: "Mark Bacchus", doctype: "Research Paper", category:"Cyber Security", keywords:"exploits", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Creating%20Exploits%20-%20EEL%206931.pdf"}
     create(document_params)
-    document_params = {title: "Home Automation Security and Vulnerabilities", author: "Henry Flefel", doctype: "research paper", category:"cyber security", keywords:"home automation security vulnerabilities", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Compiled%20Group%20Paper%20v2.pdf"}
+    document_params = {title: "Home Automation Security And Vulnerabilities", author: "Henry Flefel", doctype: "Research Paper", category:"Cyber Security", keywords:"home automation security vulnerabilities", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Compiled%20Group%20Paper%20v2.pdf"}
     create(document_params)
-=begin
-    Document.create(title: "Wifi security : wirless network encryption", author: "Gavin Garcia", doctype: "research paper", category:"cyber security", keywords:"wireless network encryption", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EthicalHackingProject.pdf" )
-    Document.create(title: "Software Crackers", author: "Bryan Degro", doctype: "research paper", category:"cyber security", keywords:"cracker hacker", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Researchpaper.pdf" )
-    Document.create(title: "Mobile Malware", author: "Alexander Pons", doctype: "research paper", category:"cyber security", keywords:"malware mobile", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Ethical%20Hacking%20final%20paper%202.0.pdf" )
-    Document.create(title: "Smart Grid", author: "Oniel Gordon", doctype: "research paper", category:"cyber security", keywords:"smart grid", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Smart%20Grid%20cyber%20physical%20security.pdf" )
-    Document.create(title: "The Insights into car hacking", author: "Mark Bacchus", doctype: "research paper", category:"cyber security", keywords:"car hacking", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Car%20Hacking%20-%20eel%206931%20final.pdf" )
-    Document.create(title: "Ethical hacking and counter measures", author: "Pons", doctype: "research paper", category:"cyber security", keywords:"ethical hacking", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EthicalHackingFinalDraft5PageProposal.pdf" )
-    Document.create(title: "Cloud Computing", author: "Roberto Calderon", doctype: "research paper", category:"cyber security", keywords:"cloud computing", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Ethical%20Hacking%20Group%20Proposal%20Final.pdf" )
-    Document.create(ztitle: "Hardware and Software security", author: "DaQuan Stevens", doctype: "research paper", category:"cyber security", keywords:"software hardware security", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Hardware%20and%20Software%20Security.pdf" )
-    Document.create(title: "Pharming", author: "Osvaldo Concepcion", doctype: "research paper", category:"cyber security", keywords:"pharming phishing", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/EEL4789_Project.pdf" )
-    Document.create(title: "Creating Exploits", author: "Mark Bacchus", doctype: "research paper", category:"cyber security", keywords:"exploits", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Creating%20Exploits%20-%20EEL%206931.pdf" )
-    Document.create(title: "Home Automation Security and Vulnerabilities", author: "Henry Flefel", doctype: "research paper", category:"cyber security", keywords:"home automation security vulnerabilities", pdflink:"http://web.eng.fiu.edu/~aperezpo/DHS/Std_Research/Compiled%20Group%20Paper%20v2.pdf" )
-=end
+    document_params = {title: "A Survey Of Insider Attack Detection Research", author: "Malek Ben Salem", doctype: "Research Paper", category:"Cyber Security", keywords:"insider attack", pdflink:"https://static-content.springer.com/lookinside/chp%3A10.1007%2F978-0-387-77322-3_5/000.png"}
+    create(document_params)
+    document_params = {title: "Cyber Security Of Smart Grid Communications: Risk Analysis And Experimental Testing", author: "Giovanna Dondassola", doctype: "Research Paper", category:"Cyber Security", keywords:"smart grid cyber security", pdflink:"https://static-content.springer.com/lookinside/chp%3A10.1007%2F978-3-662-45928-7_7/000.png"}
+    create(document_params)
   end
   def down
     Document.delete_all
